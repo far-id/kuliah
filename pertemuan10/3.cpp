c98c3f6852065fa91c3c6536af8b65091526d1cb
farid10243 <fgrizkywijaya@gmail.com>
2021-12-27 23:58:53 +0700

    .

--- pertemuan10/3.cpp
+++ pertemuan10/3.cpp
@@ -1,46 +1,22 @@
 #include <iostream>
 using namespace std;
 
-void inputMatrix(int matrix[4][4], string con)
+void multiply(int matrix1[9][9], int matrix2[9][9])
 {
-  int num;
-  for (int i = 0; i < 4; i++)
+  int mult[9][9];
+  for (int i = 0; i < 9; i++)
   {
-    for (int j = 0; j < 4; j++)
+    for (int j = 0; j < 9; j++)
     {
-      if (con == "matrix1")
-      {
-        printf("inputkan angka untuk matrix 1 [%d][%d]", i, j);
-        cin >> num;
-        matrix[i][j] = num;
-      }
-      else if (con == "matrix2")
-      {
-        printf("inputkan angka untuk matrix 2 [%d][%d]", i, j);
-        cin >> num;
-        matrix[i][j] = num;
-      }
+      mult[i][j] = matrix1[i][j] * matrix2[i][j];
     }
-  }
-}
-
-void multiply(int matrix1[4][4], int matrix2[4][4])
-{
-  int mult[4][4];
-  for (int i = 0; i < 4; i++)
-  {
-    for (int j = 0; j < 4; j++)
-    {
-      mult[i][j] = matrix1[i][j] + matrix2[i][j];
-    }
-    cout << endl;
   }
 
   cout << "Hasil Perkalian [\n";
-  for (int i = 0; i < 4; i++)
+  for (int i = 0; i < 9; i++)
   {
     cout << "\t[";
-    for (int j = 0; j < 4; j++)
+    for (int j = 0; j < 9; j++)
     {
       cout << mult[i][j] << ", ";
     }
@@ -51,10 +27,28 @@ void multiply(int matrix1[4][4], int matrix2[4][4])
 
 int main()
 {
-  int matrix1[4][4], matrix2[4][4];
-
-  inputMatrix(matrix1, "matrix1");
-  inputMatrix(matrix2, "matrix2");
+  int matrix1[9][9] = {
+      {1, 2, 3, 4, 5, 6, 7, 8, 9},
+      {1, 2, 3, 4, 5, 6, 7, 8, 9},
+      {1, 2, 3, 4, 5, 6, 7, 8, 9},
+      {1, 2, 3, 4, 5, 6, 7, 8, 9},
+      {1, 2, 3, 4, 5, 6, 7, 8, 9},
+      {1, 2, 3, 4, 5, 6, 7, 8, 9},
+      {1, 2, 3, 4, 5, 6, 7, 8, 9},
+      {1, 2, 3, 4, 5, 6, 7, 8, 9},
+      {1, 2, 3, 4, 5, 6, 7, 8, 9},
+  },
+      matrix2[9][9] = {
+          {1, 1, 1, 1, 1, 1, 1, 1, 1},
+          {2, 2, 2, 2, 2, 2, 2, 2, 2},
+          {3, 3, 3, 3, 3, 3, 3, 3, 3},
+          {4, 4, 4, 4, 4, 4, 4, 4, 4},
+          {5, 5, 5, 5, 5, 5, 5, 5, 5},
+          {6, 6, 6, 6, 6, 6, 6, 6, 6},
+          {7, 7, 7, 7, 7, 7, 7, 7, 7},
+          {8, 8, 8, 8, 8, 8, 8, 8, 8},
+          {9, 9, 9, 9, 9, 9, 9, 9, 9},
+      };
 
   multiply(matrix1, matrix2);
 
