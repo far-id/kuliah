39c8648e78b60b39807cc010020a3b6745bebb90
farid10243 <fgrizkywijaya@gmail.com>
2021-12-28 00:12:13 +0700

    .

--- pertemuan10/2.cpp
+++ pertemuan10/2.cpp
@@ -1,94 +1,92 @@
-#include <iostream>
-#include <limits>
-using namespace std;
+  #include <iostream>
+  #include <limits>
+  using namespace std;
 
-int inputInt()
-{
-  int x = 0;
-  while (!(cin >> x))
+  int inputInt()
   {
-    cin.clear();
-    cin.ignore(numeric_limits<streamsize>::max(), '\n');
-    cout << "Input bukan integer. coba lagi : ";
-  }
-  return x;
-}
-
-void inputMatrix(int matrix[4][4], string con)
-{
-  for (int i = 0; i < 4; i++)
-  {
-    for (int j = 0; j < 4; j++)
+    int x = 0;
+    while (!(cin >> x))
     {
-      if (con == "matrix1")
+      cin.clear();
+      cin.ignore(numeric_limits<streamsize>::max(), '\n');
+      cout << "Input bukan integer. coba lagi : ";
+    }
+    return x;
+  }
+
+  void inputMatrix(int matrix[4][4], string con)
+  {
+    for (int i = 0; i < 4; i++)
+    {
+      for (int j = 0; j < 4; j++)
       {
-        printf("inputkan angka untuk matrix 1 [%d][%d] :  ", i, j);
-        
-        matrix[i][j] = inputInt();
+        if (con == "matrix1")
+        {
+          printf("inputkan angka untuk matrix 1 [%d][%d] :  ", i, j);
+          
+          matrix[i][j] = inputInt();
+        }
+        else if (con == "matrix2")
+        {
+          printf("inputkan angka untuk matrix 2 [%d][%d] :  ", i, j);
+          
+          matrix[i][j] = inputInt();
+        }
       }
-      else if (con == "matrix2")
+    }
+  }
+
+  void sumSubt(int matrix1[4][4], int matrix2[4][4])
+  {
+    int sum[4][4], subt[4][4];
+    for (int i = 0; i < 4; i++)
+    {
+      for (int j = 0; j < 4; j++)
       {
-        printf("inputkan angka untuk matrix 2 [%d][%d] :  ", i, j);
-        
-        matrix[i][j] = inputInt();
+        sum[i][j] = matrix1[i][j] + matrix2[i][j];
       }
     }
-  }
-}
-
-void sumSubt(int matrix1[4][4], int matrix2[4][4])
-{
-  int sum[4][4], subt[4][4];
-  for (int i = 0; i < 4; i++)
-  {
-    for (int j = 0; j < 4; j++)
+    for (int i = 0; i < 4; i++)
     {
-      sum[i][j] = matrix1[i][j] + matrix2[i][j];
+      for (int j = 0; j < 4; j++)
+      {
+        subt[i][j] = matrix1[i][j] - matrix2[i][j];
+      }
     }
-    cout << endl;
-  }
-  for (int i = 0; i < 4; i++)
-  {
-    for (int j = 0; j < 4; j++)
-    {
-      subt[i][j] = matrix1[i][j] - matrix2[i][j];
-    }
-    cout << endl;
-  }
 
-  cout << "Hasil Penjumlahan [\n";
-  for (int i = 0; i < 4; i++)
-  {
-    cout << "\t[";
-    for (int j = 0; j < 4; j++)
+    cout << "Hasil Penjumlahan [\n";
+    for (int i = 0; i < 4; i++)
     {
-      cout << sum[i][j] << ", ";
+      cout << "\t[";
+      for (int j = 0; j < 4; j++)
+      {
+        cout << sum[i][j] << ", ";
+      }
+      cout << "]\n";
+    }
+    cout << "]\n";
+
+    cout << "Hasil Pengurangan[\n";
+    for (int i = 0; i < 4; i++)
+    {
+      cout << "\t[";
+      for (int j = 0; j < 4; j++)
+      {
+        cout << subt[i][j] << ", ";
+      }
+      cout << "]\n";
     }
     cout << "]\n";
   }
-  cout << "]\n";
 
-  cout << "Hasil Pengurangan[\n";
-  for (int i = 0; i < 4; i++)
+  int main()
   {
-    cout << "\t[";
-    for (int j = 0; j < 4; j++)
-    {
-      cout << subt[i][j] << ", ";
-    }
-    cout << "]\n";
+    int matrix1[4][4], matrix2[4][4];
+
+    inputMatrix(matrix1, "matrix1");
+    inputMatrix(matrix2, "matrix2");
+
+    sumSubt(matrix1, matrix2);
+
+    return 0;
   }
-  cout << "]\n";
-}
-
-int main()
-{
-  int matrix1[4][4], matrix2[4][4];
-
-  inputMatrix(matrix1, "matrix1");
-  inputMatrix(matrix2, "matrix2");
-
-  sumSubt(matrix1, matrix2);
-
-  return 0;
-}
