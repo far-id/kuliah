5f73fa2ab0a5be733044cb106ed7690bd84c40fe
farid10243 <fgrizkywijaya@gmail.com>
2022-05-29 11:51:15 +0700

    .

--- smt2/tugasakhir/index.cpp
+++ smt2/tugasakhir/index.cpp
@@ -8,7 +8,7 @@ void quickSort(int arr[], int left, int right) {
     int pivot = arr[(left + right) / 2];
     while (i <= j) {
         while (arr[i] < pivot)
-            i++;
+            i++;    
         while (arr[j] > pivot)
             j--;
         if (i <= j) {
@@ -18,7 +18,7 @@ void quickSort(int arr[], int left, int right) {
             i++;
             j--;
         }
-    };
+    }
     if (left < j)
         quickSort(arr, left, j);
     if (i < right)
